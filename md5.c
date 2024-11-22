46b6ac650c22a56697eced6ac90c235113f207dc
这个名字不知道能不能修改 <sdfwds4@163.com>
2024-11-22 11:35:48 +0800

    Update md5.c, support file's argument

--- md5.c
+++ md5.c
@@ -7,7 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
- 
+#include <errno.h>
+
 // Constants are the integer part of the sines of integers (in radians) * 2^32.
 const uint32_t k[64] = {
 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee ,
@@ -147,28 +148,39 @@ void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest) {
 }
  
 int main(int argc, char **argv) {
-    char *msg;
-    size_t len;
     int i;
     uint8_t result[16];
  
     if (argc < 2) {
-        printf("usage: %s 'string'\n", argv[0]);
+        printf("usage:\n    %s -f file ...\n    %s string ...\n", argv[0], argv[0]);
         return 1;
     }
-    msg = argv[1];
  
-    len = strlen(msg);
- 
-    // benchmark
-    for (i = 0; i < 1000000; i++) {
+ 	if(argc == 3 && strcmp(argv[1], "-f") == 0) {
+        FILE *fp;
+        fp = fopen(argv[2], "rb");
+        if(fp == NULL) {
+            fprintf(stderr, "File %s not exists, errno = %d, error = %s\n", argv[2], errno, strerror(errno));
+            return 1;
+        }
+        fseek(fp, 0, SEEK_END);
+        long length = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        char *data = (char*)malloc(length + 1);
+        fread(data, 1, length, fp);
+        data[length] = '\0';
+        fclose(fp);
+        md5((uint8_t*)data, length, result);
+        free(data);
+	} else {
+        char *msg = argv[1];
+        size_t len = strlen(msg);
         md5((uint8_t*)msg, len, result);
     }
- 
+
     // display result
     for (i = 0; i < 16; i++)
         printf("%2.2x", result[i]);
-    puts("");
  
     return 0;
 }
